3ca5df5c3e219c456b16c6ab15ed154582be2117
BRCurrie <BenjaminRussellCurrie@gmail.com>
2021-03-20 19:20:19 -0500

    Install @NgRx packages, verify DevTools working

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,30 @@
 import { NgModule, Optional, SkipSelf } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./store/reducers";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+// StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      },
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+  ],
 })
 export class CoreModule {
   constructor(
